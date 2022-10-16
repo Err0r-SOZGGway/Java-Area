b279cfd34e0ac7e31cde2c9db0a40d838f93bbb9
Err0r-SOZGGway <syado6664@gmail.com>
2022-10-16 15:02:11 +0900

    First Commit.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		double rand = Math.random() * 6 + 1;
+		//System.out.println(rand);
+		int number = (int)rand;
+		System.out.println("サイコロをふりました！");
+		System.out.println(number);
+
+	}
+
+}
